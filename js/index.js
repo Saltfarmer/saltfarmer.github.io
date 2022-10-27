aebe6d3f14dd1308518d6330071976caa372f41a
Candra Gctk <candragctk17@gmail.com>
2022-10-27 08:07:14 +0200

    change of titles

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".typed").typed({
-		strings: ["Programmer", "Data Scientist", "Writer", "Gamer", "Student"],
+		strings: ["Programmer", "Data Scientist", "Writer", "Gamer"],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
